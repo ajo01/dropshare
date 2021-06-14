4bf358e79d46e159526db3fe96dfdf6a2ea059ce
ajo01 <amyjojjm2001@gmail.com>
2021-06-13 23:36:43 -0700

    link padding text inside container + background img stable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import UploadContainer from './components/UploadContainer/UploadContainer'
-import BackgroundImg from './img/background.svg'
+import React from "react";
+import UploadContainer from "./components/UploadContainer/UploadContainer";
+import BackgroundImg from "./img/background.svg";
 
 function App() {
-    return (
-        <div className="center">
-            <UploadContainer className="upload-box"/>
-            {/* <img className="background-image" src={BackgroundImg} alt="background image"></img> */}
-        </div>
-    )
+  return (
+    <div>
+      <div className="center">
+        <UploadContainer className="upload-box" />
+      </div>
+      <img className="background-image" src={BackgroundImg} alt="background image"></img>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Toast/ToastAlert.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./ToastAlert.module.css";
+
+const ToastAlert = () => {
+  return <div className={styles.toast}>Toast Message</div>;
+};
+
+export default ToastAlert;
